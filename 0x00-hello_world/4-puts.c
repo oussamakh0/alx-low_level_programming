dd7947d8f05fee3fae4711eee5db82be474bfd10
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-08-10 23:44:20 +0000

    I added a function description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 /**
- *Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
- *Use the function puts
- *You are not allowed to use printf
- *Your program should end with the value 0
+ * main:- prints "Programming is like building a multilingual puzzle
+ * Return: It retuns 0
  */
 int main(void)
 {
