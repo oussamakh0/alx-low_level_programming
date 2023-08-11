b267aa798b304ebd0bf94ddd88137fb35b60de00
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-08-11 01:08:22 +0000

    last task in adv

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,8 @@
 #include<stdio.h>
 /**
  *main - prints a message without using puts and printf
- *Description -  program that prints exactly and that piece of art is useful" 
+ *Description -  program that prints exactly and that piece of art is useful"
+ *@parameterx: an array of chars to be givven to be outputted by write
  *- Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
  * Return - value is 1 on success
  */
