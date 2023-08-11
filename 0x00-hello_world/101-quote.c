9d20d83848a491e0151909779ed320aaa5400ece
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-08-11 01:05:00 +0000

    last task in adv

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,12 @@
  *main - prints a message without using puts and printf
  *Description -  program that prints exactly and that piece of art is useful" 
  *- Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
- * Return - value is 0
+ * Return - value is 1 on success
  */
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(STDOUT_FILENO, str, 59);
 	return (1);
 }
