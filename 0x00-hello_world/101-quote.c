83511d4cbd76d9bc2a69f52db1d700bb224c9368
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-08-11 00:55:15 +0000

    last task in adv

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include<stdlib.h>
 #include<unistd.h>
 #include<stdio.h>
 /**
@@ -8,7 +9,7 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar
-			, 2015-10-19", 59);
+	write(2, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
 }
