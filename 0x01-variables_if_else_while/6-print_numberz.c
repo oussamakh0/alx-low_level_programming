beb8030bb0d99a30f22831095fe2fea9ebf06f3b
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-08-12 01:52:22 +0000

    just ywo putchars and no char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - Write a program that prints all single digit
+ *numbers of base 10 starting from 0, followed by a new line.
+ *Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+
+	i = 48;
+	while (i <= 57)
+		putchar(i++);
+	putchar('\n');
+	return (0);
+}
