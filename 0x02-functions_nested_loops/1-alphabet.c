73a711610ffd7e21bfe620231ba599dde0087a8a
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-08-15 23:30:48 +0000

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *main - prints the alphabet, in lowercase, followed by a new line
+ *Return: 0 on success
+ */
+int main(void)
+{
+	print_alphabet();
+	_putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,10 +2,28 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 void func_putchar(char c)
 {
-	putchar(c);
+	_putchar(c);
+}
+
+void print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+	while(c <= 'z')
+	{
+		func_putchar(c++);
+	}
+
 }
 
 #endif
