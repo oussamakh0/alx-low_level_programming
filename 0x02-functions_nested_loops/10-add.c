60830658fe7d771eb667a86fadbfd718873fe0fb
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-08-16 02:35:27 +0000

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *add - add two parameters 
+ *@n1: number 1
+ *@n2: number 2
+ *Return: the sum of n1 and n2
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -19,4 +19,6 @@ int print_last_digit(int);
 
 void jack_bauer(void);
 
+int add(int, int);
+
 #endif
