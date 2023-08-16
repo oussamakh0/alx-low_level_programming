65ae8c2d5d581928e22e9336cce2fe77b003f624
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-08-16 01:48:27 +0000

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_sign - print the sign og the number
+ *@n: number to be checked
+ *Return: either + or - or 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar('1');
+		return (-1);
+	}
+}
+
