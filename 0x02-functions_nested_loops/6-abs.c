524f21d9c0dc46957288f5b084a42e086c75e7da
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-08-16 01:56:09 +0000

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_abs - evluate the absolute value of parameter
+ *@n: number
+ *Return: absolute value of n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
