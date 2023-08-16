e3a24cfc4f945c4856a5ddb7146351fb4423ab25
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-08-16 02:19:56 +0000

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,11 +12,11 @@ void jack_bauer(void)
 	{
 		while (min <= 59)
 		{
-			_putchar((hor % 10) + 48);
 			_putchar((hor / 10) + 48);
+			_putchar((hor % 10) + 48);
 			_putchar(':');
-			_putchar((min % 10) + 48);
 			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
 			_putchar('\n');
 			min++;
 		}
