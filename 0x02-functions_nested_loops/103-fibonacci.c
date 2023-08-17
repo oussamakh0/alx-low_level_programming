cdd95a45447bdbff03a14c7b43b29f8e5181b585
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-08-17 17:44:33 +0000

    task 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ *main - By considering the terms in the Fibonacci
+ *sequence whose values do not exceed 4,000,000, write
+ *a program that finds and prints the sum of the even-valued
+ *terms, followed by a new line.
+ *Return: 0 on success
+ */
+
+int main(void)
+{
+	unsigned long int v1;
+	unsigned long int v2;
+	unsigned long int v3;
+	unsigned long int count;
+
+	v1 = 0;
+	v2 = 1;
+	v3 = v1 + v2;
+	count = 0;
+	while (v3 <= 4000000)
+	{
+		if (v3 % 2 == 0)
+			count += v3;
+		v1 = v2;
+		v2 = v3;
+		v3 = v1 + v2;
+	}
+	printf("\n");
+	return (0);
+}
