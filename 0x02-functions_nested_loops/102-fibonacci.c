e7ffb32900f5d79ecc011a9478e9bc2e295bb1bb
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-08-17 17:23:10 +0000

    task 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,38 +1,29 @@
-#include "main.h"
 #include <stdio.h>
-
 /**
  * main - check the func
  * Return: 0
  */
-
 int main(void)
 {
+	int v1;
+	int v2;
+	int v3;
 	int count;
 
-	long int first, second;
-	long int next;
 
-	count = 3;
-	first = 1;
-	second = 2;
-	next = first + second;
-	printf("%lu, ", first);
-	printf("%lu, ", second);
-	while (count <= 50)
+	v1 = 0;
+	v2 = 1;
+	v3 = v1 + v2;
+	count = -1;
+	while (++count < 50)
 	{
-		if (count == 50)
-		{
-			printf("%lu \n", next);
-		}
-		else
-		{ 
-			printf("%lu, ", next); 
-		}
-		first = second;
-		second = next;
-		next = first + second;
-		count++;
+		printf("%d", v3);
+		v1 = v2;
+		v2 = v3;
+		v3 = v1 + v2;
+		if (count < 50)
+			printf(", ");
 	}
+	printf("\n");
 	return (0);
 }
