875e47d9c841836b724e4f3e81d3e9967153d98e
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-08-17 17:28:57 +0000

    task 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,14 +14,14 @@ int main(void)
 	v1 = 0;
 	v2 = 1;
 	v3 = v1 + v2;
-	count = -1;
-	while (++count < 50)
+	count = 1;
+	while (count++ <= 50)
 	{
 		printf("%ld", v3);
 		v1 = v2;
 		v2 = v3;
 		v3 = v1 + v2;
-		if (count < 50)
+		if (count <= 50)
 			printf(", ");
 	}
 	printf("\n");
