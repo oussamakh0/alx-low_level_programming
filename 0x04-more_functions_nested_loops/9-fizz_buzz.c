32ea4f1ada162f6775e88dc69e0a1dcf20021f2a
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-08-18 23:36:20 +0000

    task  8

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,8 @@ int main(void)
 			printf("Fizz");
 		if (i % 5 == 0)
 			printf("Buzz");
-		if (!((i % 3 == 0) ||  (i % 5 == 0)))
+		if (!((i % 3 == 0) ||
+			(i % 5 == 0)))
 			printf("%d", i);
 		printf(" ");
 	}
