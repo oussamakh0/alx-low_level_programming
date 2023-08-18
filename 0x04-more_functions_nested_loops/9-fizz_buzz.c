bd6dfa5bb5b6239f47caf3672a6ed3af9d0d6ab7
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-08-18 23:34:06 +0000

    task  8

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - check the code, hint: Fizz fuzz
+ *Return: none on success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if (!((i % 3 == 0) ||  (i % 5 == 0)))
+			printf("%d", i);
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
