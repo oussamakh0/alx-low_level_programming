6c76bb2fd0cd5211dc2d5468a90ac0028158b05a
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-08-18 01:37:34 +0000

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_line - print n times _
+ *@n: the number of _ will be printed
+ *Return: none
+ */
+
+void print_line(int n)
+{
+	while (n != 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}
