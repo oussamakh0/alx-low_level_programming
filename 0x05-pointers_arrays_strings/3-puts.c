43968df72bec9449684828d05041a485eaa7b5cb
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-08-21 15:59:28 +0000

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- *_puts - function that prints a string,
- *followed by a new line, to stdout.
- *@str: arg to be outputed
- *Return: nothing
+ *_puts - prints string s to stdout
+ *@s: arg to be outputed
+ *Return: none on success
  */
 
-void _puts(char *str)
+void _puts(char *s)
 {
 	int i;
 
 	i = 0;
 	while (s[i] != '\0')
-		write(1, &s[i++], 1);
-	write(1, '\n', 1);
+		_putchar(s[i++]);
+	_putchar('\n');
 }
