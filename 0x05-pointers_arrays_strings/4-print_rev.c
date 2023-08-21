31fba55df06c5837f3ffec230b76026147efcdb2
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-08-21 16:17:46 +0000

    task 4 a function that prints an str in reverse order

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -17,3 +17,19 @@ void print_rev(char *s)
 			_putchar(s[len]);
 	}
 }
+
+/**
+ *_strlen - counts strings charachters = length
+ *@s: arg is the string to be counted
+ *Return: i the string length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
