5f439fba3bc93a1863b089ff462f9839e3ed2d6d
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-08-21 19:12:19 +0000

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = -1;
+	while (++i < n)
+	{
+		printf("%d", *(a + i));
+		if (i != n)
+			printf(" ,");
+	}
+	_putchar('\n');
+}
