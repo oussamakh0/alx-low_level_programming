cf9ddd45e45a1d4d0eb37ef06707eaac4c60252d
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-08-21 18:00:12 +0000

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,12 +10,30 @@
 void puts2(char *str)
 {
 	int i;
+	int len;
 
+	len  = _strlen(str);
 	i = 0;
-	while (str[i]  != '\0')
+	while (i < len)
 	{
 		_putchar(str[i]);
 		i += 2;
 	}
 	_putchar('\n');
+}
+
+/**
+ *_strlen - counts strings charachters = length
+ *@s: arg is the string to be counted
+ *Return: i the string length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
 }
