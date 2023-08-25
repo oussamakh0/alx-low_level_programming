4885687577de1ab8d7c05f9018b048c4818c3df8
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-08-25 01:26:20 +0000

    now ok

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,18 +1,29 @@
 #include "main.h"
 
+
 /**
- *
- *
- *
+ * leet - function that encodes a string into 1337
+ * @str: character to be checked
+ * Return: if seperator return 1. Otherwise return 0;
  */
+
 
 char *leet(char *str)
 {
-	int i;
+	int count = 0, i = 0;
+	char *letters = "aeotlAEOTL";
+	char *nums = "4307143071";
 
-	i = 0;
-	while (str[i] != '\0')
+	while (str[count] != '\0')
 	{
-
+		i = 0;
+		while (letters[i] != '\0')
+		{
+			if (str[count] == letters[i])
+				str[count] = nums[i];
+			i++;
+		}
+		count++;
 	}
+	return (str);
 }
