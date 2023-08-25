c8b89f72999272041274988fa29a256041e7781a
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-08-25 23:04:38 +0000

    magic

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - program prints a[2] = 98
+ *followed by a new line.
+ *Return: 0 on success
+ */
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	p[5] = 98;
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
