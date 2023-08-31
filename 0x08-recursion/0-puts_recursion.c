786a57c08743fb7098318f635fc959618f2f85a4
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-08-31 23:08:26 +0000

    task 0 empty string case handling

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,8 @@
 #include "main.h"
 
 
+
+
 /**
  *_puts_recursion - function that prints a string
  *followed by a new line.
@@ -9,17 +11,21 @@
  */
 
 
+
+
 void _puts_recursion(char *s)
 {
 	int i;
 
 	i = 0;
+	if (*s != '\0')
+		_putchar(*s);
+	i++;
 	if (*(s + i) == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	i++;
 	else
 		_puts_recursion(s + i);
 }
