c689bd22ba60191cac3e6ac2d469655231e8d8fd
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-09-01 00:29:00 +0000

    task  2

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion - print parameter s in reverse
+ *@s: to be reversed and printed
+ *Return: nothing on succes
+ */
+
+void _print_rev_recursion(char *s)
+{
+
+}
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_strlen_recursion - count a string lenght
+ *@s: its lenght to be counted
+ *Return: the lenght of s
+ */
+
+
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	len = 0;
+	if (*(s + len) == '\0')
+		return;
+	else if (*(s + len) != '\0')
+	{
+		len++;
+		len += _strlen_recursion(s + len);
+	}
+	return (len);
+}
+
