20e10f10cc5d4afb791a82566bc1a9b54cd63d05
oussamaelkh0 <elkhairioussama0@gmail.com>
2023-09-01 02:40:03 +0000

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,12 +1,15 @@
 #include "main.h"
-
 /**
  *_print_rev_recursion - print parameter s in reverse
  *@s: to be reversed and printed
  *Return: nothing on succes
  */
-
 void _print_rev_recursion(char *s)
 {
-
+	if (*s == '\0')
+		return;
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }
