da684e1764cfcf8d490850bc71b23f9de1ad51b0
oussamakh0 <elkhairioussama0@gmail.com>
2023-10-02 04:17:10 -0700

    I modified the comment + I have added the main declaration in main.h

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,14 @@
 /**
  * main - Write a program that prints the number of arguments passed into it
- * Description: Write a program that prints the number of arguments
- * passed into it. the program should print a number, followed by a new line
  * @argc: argument counter
  * @argv: argument vector
+ * Description: Write a program that prints the number of arguments
+ * passed into it. the program should print a number, followed by a new line
  * Return: 0 on success
 */
 
 #include <stdio.h>
+#include "main.h"
 
 int main(int argc, char **argv)
 {
