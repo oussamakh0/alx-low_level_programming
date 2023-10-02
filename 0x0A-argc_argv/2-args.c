5bc8cebd43dd5687debb580894cf01f41ef2974a
oussamakh0 <elkhairioussama0@gmail.com>
2023-10-02 05:52:29 -0700

    Write a program that prints all arguments it receives.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints all arguments it receives.
+ * Description: All arguments should be printed, including the first one
+ * Only print one argument per line, ending with a new line
+ * @argc: argument counter
+ * @argv: arguments vector
+ * Return: 0 on success.
+*/
+
+int main(int argc, char **argv)
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
+}
