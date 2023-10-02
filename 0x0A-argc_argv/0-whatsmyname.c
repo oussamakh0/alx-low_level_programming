a4747afee4ba51b73b594a7d066cf943ca5ee2af
oussamakh0 <elkhairioussama0@gmail.com>
2023-10-02 04:01:20 -0700

    some betty comments changes

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,11 @@
 /**
- *Main - Write a program that prints its name, followed by a new line
- *@param1: argc for argument counts
- *@param2: argv for argument vector
+ *main - Write a program that prints its name, followed by a new line
+ *@argc: for argument counts
+ *@argv: for argument vector
  *Description: If you rename the program, it will print the new name,
  *without having to compile it again
  *You should not remove the path before the name of the program
- *Return -  0 on success
+ *Return: 0 on success
  */
 
 #include <stdio.h>
