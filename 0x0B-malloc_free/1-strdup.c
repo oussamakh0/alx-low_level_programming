a6c17221232183a79698079f1522cb586fde5ebb
oussamakh0 <elkhairioussama0@gmail.com>
2023-10-04 17:07:13 -0700

    Write a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,15 +1,45 @@
 #include <stdlib.h>
 
 /**
- *_strdup - function that returns a pointer to a newly allocated
- *space in memory, which contains a copy of the string
- *given as a parameter.
- *@str:	the string to be dupplicated.
- *Return:NULL if str = NULL, or a  pointer to the duplicated string
+ * _strlen - counts string length
+ * @s: the string's legth to be counted
+ * Return: the length of s.
 */
 
-void _strcpy(char *s1, char *s2);
-size_t _strlen(char *s);
+size_t _strlen(char *s)
+{
+        size_t s_len;
+
+        s_len = 0;
+        while (s[s_len] != '\0')
+                s_len++;
+        return (s_len);
+}
+
+/**
+ * _strcpy - copy s1 to s2
+ * @s1: string to be coppied from
+ * @s2: string to be copied into
+ * Return: nothing
+*/
+
+void _strcpy(char *s1, char *s2)
+{
+        while (*s1 != '\0')
+        {
+                *s2 = *s1;
+                s1++;
+                s2++;
+        }
+        *s2 = '\0';
+}
+
+/**
+ *_strdup - function that returns a pointer to a newly allocated
+ *space in memory, which contains a copy of the string given as a parameter.
+ *@str: the string to be dupplicated.
+ *Return:NULL if str = NULL, or a  pointer to the duplicated string
+*/
 
 char *_strdup(char *str)
 {
@@ -26,38 +56,4 @@ char *_strdup(char *str)
 		return (dup_str);
 	_strcpy(str, dup_str);
 	return (dup_str);
-}
-
-/**
- * _strlen - counts string length
- * @s: the string's legth to be counted
- * Return: the length of s.
-*/
-
-size_t _strlen(char *s)
-{
-	size_t s_len;
-
-	s_len = 0;
-	while (s[s_len] != '\0')
-		s_len++;
-	return (s_len);
-}
-
-/**
- * _strcpy - copy s1 to s2
- * @s1: string to be coppied from
- * @s2: string to be copied into
- * Return: nothing
-*/
-
-void _strcpy(char *s1, char *s2)
-{
-	while (*s1 != '\0')
-	{
-		*s2 = *s1;
-		s1++;
-		s2++;
-	}
-	*s2 = '\0';
 }
