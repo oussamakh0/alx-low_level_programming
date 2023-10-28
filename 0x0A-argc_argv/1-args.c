0de2b6dd982d04a6c2ce16c8651f4e97fba1251e
oussamakh0 <elkhairioussama0@gmail.com>
2023-10-27 19:03:50 -0700

    little fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Write a program that prints the number of arguments passed into it
+ * main - Write a program that prints the number of arguments
+ * passed into it.
  * @argc: argument counter
  * @argv: argument vector
- *
  * Return: 0 on success.
 */
 
 int main(int argc, char **argv)
 {
-	__attribute__((unused)) argc;
+	(void)argc;
 	printf("%d\n", argc - 1);
 	return (0);
 }
