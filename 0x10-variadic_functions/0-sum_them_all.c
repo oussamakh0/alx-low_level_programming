894ddb87d1251f93bdfa08b008e8e0212b553f61
oussamakh0 <elkhairioussama0@gmail.com>
2023-11-10 02:56:15 +0100

    I changed the headerfile name in 0-... file and in the headerfile itself

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,7 +1,8 @@
-#include "var.func.h"
+#include "variadic_functions.h"
 
 /**
  * sum_them_all - function that returns the sum of all its parameters.
+ *
  * @n: the last fixed param or the number of params
  * Return: sum or 0.
  */
@@ -12,8 +13,6 @@ int sum_them_all(const unsigned int n, ...)
 	int sum;
 	unsigned int i;
 
-	if (!n)
-		return (0);
 	va_start(ap, n);
 	i = 0;
 	sum = 0;
---
+++ 0x10-variadic_functions/main.c
@@ -0,0 +1,6 @@
+#include <stio.h>
+
+int main()
+{
+
+}
