1a8cb375c3491fa68c8506385631e15fe2456471
oussamakh0 <41795158+oussamakh0@users.noreply.github.com>
2023-12-16 02:58:15 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,16 +5,17 @@
 */
 int main(void)
 {
-	int c = -1;
+	int i = 0;
 
-	while (c++ < 10)
+	while (i < 10)
 	{
-		putchar(48 + c);
-		if (c != 9)
+		putchar(i + '0');
+		if (i < 9)
 		{
-			putchar(',');
-			putchar(' ');
+			putchar(44);
+			putchar(32);
 		}
+		i++;
 	}
 	putchar('\n');
 	return (0);
