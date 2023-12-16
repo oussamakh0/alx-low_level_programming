89bb77623391561c088bf536cd66c72ccfda76e2
oussamakh0 <41795158+oussamakh0@users.noreply.github.com>
2023-12-16 02:55:38 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,19 +3,18 @@
  *main - Write a program that prints all combinations of single digs
  *Return: 0 on success
 */
-
 int main(void)
 {
-	int num;
+	int c = -1;
 
-	num = 0
-	for (; num <= 9; num++)
+	while (c++ < 10)
 	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-		putchar(',');
-		putchar(' ');
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
